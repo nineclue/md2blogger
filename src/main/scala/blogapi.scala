edd6a47ae5a69857c13d3ce72e4ff5d934773f9b
Suhku Huh <nineclue@gmail.com>
2014-01-20 14:37:56 +0900

    1) some code cleanups 2) introduced Post class

--- src/main/scala/blogapi.scala
+++ src/main/scala/blogapi.scala
@@ -5,7 +5,7 @@ import java.net.URL
 import java.nio.file.{Path, Paths, Files, LinkOption}
 import scala.pickling._
 import json._
-import play.api.libs.json.Json
+import play.api.libs.json.{Json, JsObject, JsNumber, JsArray, JsString, JsValue}
 
 object MiniParser {
 	val stringJson = new scala.util.matching.Regex("""\s*"(.*?)"\s*:\s*"(.*?)"\s*,?\s*""", "key", "value")
@@ -17,6 +17,8 @@ object MiniParser {
 	}
 }
 
+case class Post(title:String, content:String, id:Option[String]=None, url:Option[String]=None, selfLink:Option[String]=None)
+
 object BlogAPI {
   import collection.JavaConversions.asScalaBuffer
 
@@ -25,6 +27,8 @@ object BlogAPI {
 	private val tokenUrl = new URL("https://accounts.google.com/o/oauth2/token")
 	private var bloggerData = Map.empty[String, String]
 
+	private val blogID = "blog_id"
+	private val blogName = "blog_name"
 	private val clientID = "client_id"
 	private val clientSecret = "client_secret"
 	private val accessToken = "access_token"
@@ -45,37 +49,54 @@ object BlogAPI {
 		val ac = System.console.readLine
 
 		// get access token
-		val req = request.RequestBody(Map("code"->ac,
-							"client_id"->id, "client_secret"->secret,
+		val req = request.RequestBody(Map("code" -> ac,
+							clientID -> id, clientSecret -> secret,
 							"redirect_uri"->"urn:ietf:wg:oauth:2.0:oob", "grant_type"->"authorization_code"))
 		val client = new HttpClient
 		val resp = client.post(tokenUrl, Some(req))
-		bloggerData = MiniParser.parse(resp).filterKeys(k => (k == "access_token" || k == "refresh_token")) + 
-			(("client_id", id), ("secret", secret))
+		bloggerData = MiniParser.parse(resp).filterKeys(k => (k == accessToken || k == refreshToken)) +
+			((clientID, id), (clientSecret, secret))
 		println(s"Successfully got tokens.")
 
 		// get user's blog
+		val blogs = blogsList
+		val bid =
+			if (blogs.size == 1) {
+				println(s"Mananging blog ${blogs(0)._2}")
+				0
+			} else {
+				println("Found more than one blogs, ")
+				(1 to blogs.size).zip(blogs).foreach {
+					case (i, b) =>
+						println(s"  $i : ${b._2} - ${b._3} posts")
+				}
+				print("Enter the number of blog you want to manage : ")
+				System.console.readLine.toInt - 1
+			}
+		bloggerData = bloggerData + ((blogID, blogs(bid)._1), (blogName, blogs(bid)._2))
 		saveData
 	}
 
 	def saveData = {
 		val file = Files.newBufferedWriter(Paths.get(fileName), java.nio.charset.Charset.forName("UTF-8"))
-    try 
+    try
       file.write(bloggerData.pickle.value)
     finally
-      file.close		
+      file.close
 	}
 
-	def listBlogs:Unit = {
+	def blogsList:Array[(String, String, Int)] = {
 		val client = new HttpClient
-		val head = header.Headers(Map("Authorization" -> ("Bearer " + bloggerData.get("access_token").get)))
+		val head = header.Headers(Map("Authorization" -> ("Bearer " + bloggerData.get(accessToken).get)))
 		val byUrl = new URL("https://www.googleapis.com/blogger/v3/users/self/blogs")
 		val response = client.get(byUrl, head)
 		if (response.status.code == 401) {
 			updateAccessToken
-			listBlogs
+			blogsList
+		} else {
+			val json = Json.parse(response.body.toString)
+			json.\("items").as[Array[JsValue]].map(b => (b.\("id").as[String], b.\("name").as[String], b.\("posts").\("totalItems").as[Int]))
 		}
-		val json = Json.parse(response.body.toString)
 	}
 
 	def load = {
@@ -86,21 +107,21 @@ object BlogAPI {
 		} else {
 			initialize
 		}
-		println(bloggerData)
-		listBlogs
 	}
 
 	def updateAccessToken = {
 		println("Refreshing access token.")
 		val client = new HttpClient
-		val head = header.Headers(Map("Authorization" -> ("Bearer " + bloggerData.get("refresh_token").get)))
-		val req = request.RequestBody(Map("client_id" -> bloggerData.get("client_id").get,
-			"client_secret" -> bloggerData.get("client_secret").get, 
-			"refresh_token" -> bloggerData.get("refresh_token").get, "grant_type" -> "refresh_token"))
+		val head = header.Headers(Map("Authorization" -> ("Bearer " + bloggerData.get(refreshToken).get)))
+		val req = request.RequestBody(Map(clientID -> bloggerData.get(clientID).get,
+			clientSecret -> bloggerData.get(clientSecret).get,
+			refreshToken -> bloggerData.get(refreshToken).get, "grant_type" -> "refresh_token"))
 		val resp = client.post(tokenUrl, Some(req))
-		bloggerData = bloggerData.updated("access_token", MiniParser.parse(resp).get("access_token").get)
+		bloggerData = bloggerData.updated(accessToken, MiniParser.parse(resp).get(accessToken).get)
 		saveData
 	}
+
+	def addPost(post:Post):Post = post
 
 	def tokens = bloggerData
 }
